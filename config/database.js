b4df975ce31a4f510c6cbe6c1935fd5b38f1da03
catalina <catapf493@yahoo.com>
2018-03-18 10:53:32 -0500

    adding dockerfiles and dockercompose to set up the mean app tracker-v2

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
     // `27017` is mongo's default port
     // `trackermateo` is the db's name
-    database: 'mongodb://localhost:27017/trackermateo',
+    // database: 'mongodb://localhost:27017/trackermateo',
+    database: 'mongodb://database/tracker-v2',
     // the secret for our token
     secret: "secretirijillo"
 };
